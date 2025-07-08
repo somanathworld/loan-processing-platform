1c8530c077a05ef8a1dec57187f654139bee46b0
Somanath Mishra <84952279+somanathworld@users.noreply.github.com>
2025-07-08 15:17:30 +0000

    adding ms

---
+++ user-service/src/main/java/com/gs/controller/UsersController.java
@@ -0,0 +1,15 @@
+package com.gs.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @GetMapping("/status")
+    public String getUserStatus() {
+        return "User Service is running";
+    }
+}
