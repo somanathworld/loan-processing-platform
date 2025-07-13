6f7bbec94f9735102b6f65c4358c6ece304bfe96
Somanath Mishra <84952279+somanathworld@users.noreply.github.com>
2025-07-13 15:03:27 +0000

    updating jar file

--- rest-consumer/src/main/java/com/example/rest_consumer/RestConsumerApplication.java
+++ rest-consumer/src/main/java/com/example/rest_consumer/RestConsumerApplication.java
@@ -8,6 +8,26 @@ public class RestConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestConsumerApplication.class, args);
+
+		 RestConsumer client = new RestConsumer();
+        client.register("john1", "12345");
+        String token = client.login("john1", "12345");
+        if (token != null) {
+            System.out.println("Login successful, token: " + token);
+             client.getProtectedResource(token);
+            client.getProtectedResource2(token);
+            client.getProtectedResource3(token);
+            System.out.println(client.registerCustomer(token));
+            System.out.println(
+                    client.uploadKYC(token, "68714419089e5f073d531f41",
+                            "/workspaces/loan-processing-platform/docker-compose.yml"));
+            System.out.println(client.loanStatus(token));
+            System.out.println(client.applyForLoan(token, "12344", "2344L", 30000.00));
+
+        } else {
+            System.out.println("Login failed");
+        }
+
 	}
 
 }
